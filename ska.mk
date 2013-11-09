08b8edea7a578aa4c5787a4d6132b76e8684acd3
XsMagical <XsMagical@team-nocturnal.net>
2013-11-09 08:14:54 -0500

    Update and rename cm.mk to ska.mk

--- ska.mk
+++ ska.mk
@@ -8,6 +8,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.2.2/JDQ39/M919UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.2.2 JDQ39 M919UVUAMDB release-keys"
 
-PRODUCT_NAME := cm_jfltetmo
+PRODUCT_NAME := ska_jfltetmo
 PRODUCT_DEVICE := jfltetmo
 
